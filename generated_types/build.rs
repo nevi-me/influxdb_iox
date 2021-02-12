fbc0e19d04e1359fc717448bc456bd84c24e9a49
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2021-02-12 17:19:37 +0000

    refactor: consistent protobuf topology

--- generated_types/build.rs
+++ generated_types/build.rs
@@ -10,7 +10,7 @@ type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 fn main() -> Result<()> {
-    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("protos");
 
     generate_grpc_types(&root)?;
     generate_wal_types(&root)?;
@@ -21,15 +21,18 @@ fn main() -> Result<()> {
 /// Schema used with IOx specific gRPC requests
 ///
 /// Creates `influxdata.platform.storage.rs` and
-/// `com.github.influxdata.idpe.storage.read.rs`
+/// `com.github.influxdata.idpe.storage.read`
 fn generate_grpc_types(root: &Path) -> Result<()> {
+    let storage_path = root.join("influxdata/platform/storage");
+    let idpe_path = root.join("com/github/influxdata/idpe/storage/read");
+
     let proto_files = vec![
-        root.join("test.proto"),
-        root.join("predicate.proto"),
-        root.join("storage_common.proto"),
-        root.join("storage_common_idpe.proto"),
-        root.join("service.proto"),
-        root.join("source.proto"),
+        storage_path.join("test.proto"),
+        storage_path.join("predicate.proto"),
+        storage_path.join("storage_common.proto"),
+        storage_path.join("service.proto"),
+        storage_path.join("source.proto"),
+        idpe_path.join("storage_common_idpe.proto"),
     ];
 
     // Tell cargo to recompile if any of these proto files are changed
